271365d281f8c6e047966d72770ff38a6a6a0c7e
ogonkem <ogonkem2000@yahoo.com>
2021-03-29 21:38:52 +0100

    correct requirements.txt

--- config/prod.py
+++ config/prod.py
@@ -3,5 +3,5 @@ import os
 
 DEBUG =False
 SECRET_KEY=secrets.token_hex(32)
-SQLALCHEMY_DATABASE_URI= os.environ['DATABASE_URL']
+SQLALCHEMY_DATABASE_URI= os.environ['DATABASE_URL'].replace("://", "ql://", 1)
 SQLALCHEMY_TRACK_MODIFICATIONS=False
