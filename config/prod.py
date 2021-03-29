9a36be36f20e74a112e37b3f36cddc871f8eff17
ogonkem <ogonkem2000@yahoo.com>
2021-03-29 12:01:08 +0100

    Updated Prod.py file

--- config/prod.py
+++ config/prod.py
@@ -1,6 +1,7 @@
 import secrets
+import os
 
 DEBUG =False
 SECRET_KEY=secrets.token_hex(32)
-SQLALCHEMY_DATABASE_URI='postgresql://postgres:Ogo1pet2andy3@localhost/catalog_db'
+SQLALCHEMY_DATABASE_URI= os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS=False
