ff6acd04e3dfa64e84dc2dbe8c2137a686d19cca
Nwachukwu Ogochukwu <ogonkem2000@yahoo.com>
2021-04-30 10:07:36 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,16 @@
 from app import create_app, db  # app/__init__ (ROOT PACKAGE)
 from app.auth.models import User
 
+#-------------------------------Production--------------------------------
+# flask_app = create_app('prod')
+# with flask_app.app_context():
+#     db.create_all()
+#     try:
+#         if not User.query.filter_by(user_name='harry').first():
+#             User.create_user(user='harry', email='harry@hogwarts.com', password='secret')
+#     except exc.IntegrityError:
+#             flask_app.run()
+#-------------------------------Development--------------------------------
 if __name__ == '__main__':
     flask_app = create_app('prod')
     with flask_app.app_context():
